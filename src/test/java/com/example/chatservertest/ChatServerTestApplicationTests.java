c53ee7537fd3ea8752be0becf72501a9cd241522
kuldeep_mac <kuldeep281293@gmail.com>
2024-04-21 12:55:48 +0400

    initiate empty project for chatRoom Server

---
+++ src/test/java/com/example/chatservertest/ChatServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
